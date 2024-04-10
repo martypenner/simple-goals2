e2fd9c64ca3a9b027e96072e6e33b3dd4be8b276
Marty Penner <martypenner@users.noreply.github.com>
2024-04-10 19:11:22 -0400

    feat: add deploy steps

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
-import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
   build: {
     target: 'esnext',
   },
