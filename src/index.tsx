3bfb6e0dcc19abdc362fc65fe357d30951339725
Marty Penner <martypenner@users.noreply.github.com>
2024-04-29 20:02:41 -0400

    feat: add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,23 @@
+import * as Sentry from '@sentry/react';
 import { Goals } from '@/components/goals';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { r } from './reflect';
+
+Sentry.init({
+  dsn: 'https://0d68d0e6adb64123949409263a98be20@o4506871392632832.ingest.us.sentry.io/4507172546019328',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/simple-goals\.pages\.dev/],
+  // Session Replay
+  replaysSessionSampleRate: 1.0, // 100% sample rate
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 import './globals.css';
 import { useInitialized } from './subscriptions';
