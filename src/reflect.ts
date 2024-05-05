f5bc3ed2653b0d67b583ccba8c0ae9ebd3fb0368
Marty Penner <martypenner@users.noreply.github.com>
2024-05-05 12:19:44 -0400

    tracing: log the auth cookie for better tracing

--- src/reflect.ts
+++ src/reflect.ts
@@ -11,6 +11,7 @@ const authCookie = document.cookie
   .split('; ')
   .map((entry) => entry.split('='))
   .find(([key]) => key === 'CF_Authorization')?.[1];
+console.log(document.cookie);
 if (import.meta.env.PROD && authCookie == null) {
   // We may be logged in, but the cookie may have been deleted while we're sitting on the page.
   // Refresh to re-authenticate.
